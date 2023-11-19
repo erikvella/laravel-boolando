8b905661d9eb517124fdfa4559a4b23504fbbb07
erikvella <vella.erik1@gmail.com>
2023-11-19 14:52:18 +0100

    [to fix] home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,29 @@
 @section('content')
     <div class="container-main d-flex ">
 
+        @forelse ($products as $product)
+            <div class="card">
 
-        <h1>questa è la homepage</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus inventore beatae pariatur exercitationem
-            voluptatum necessitatibus. Sapiente quis labore ut repudiandae reiciendis quae ipsa, ad ipsam corrupti, in sequi
-            accusantium alias enim tenetur quasi deleniti culpa nostrum asperiores dicta, quam consequatur! Quos architecto
-            ratione iure sint porro reiciendis aut accusantium numquam!</p>
+
+                <img class="img1" src="/assets/{{ $product['frontImage'] }}" alt="">
+                <img class="img2" src="/assets/{{ $product['backImage'] }}" alt="1b">
+
+                <span v-if="product.discount" class="discount">{{ $product['name'] }}</span>
+
+                <span class="heart">&hearts;</span>
+
+                <div class="descr">
+                    <span class="marca">{{ $product['brand'] }}</span>
+                    <h3>{{ $product['brand'] }}</h3>
+                    <span class="red-price">&euro;{{ $product['price'] }}</span>
+                    <span v-if="product.fullPrice" class="barred-price">&euro;{{ $product['price'] }}</span>
+                </div>
+
+            </div>
+
+        @empty
+            <h2>Non ci sono prodotti</h2>
+        @endforelse
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    // carico da config l'array di prodotti da mostrare
+    $products = config('products');
+
+    return view('home' ,compact('products'));
 })->name('home');
 
 Route::get('/dettagli-prodotto', function () {
