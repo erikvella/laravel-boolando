c87cafd3a3dc03300296455af7d47d15042481c2
erikvella <vella.erik1@gmail.com>
2023-11-21 18:12:23 +0100

    [fix] price discount

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,34 @@
 @extends('layouts.main')
 
+
 @section('content')
     <div class="container-main d-flex ">
 
+
         @forelse ($products as $product)
             <div class="card">
+                @php
 
+                    $disc_price = $product['price'] * (50 / 100);
+                    $format_price_disc = number_format($disc_price, 2);
+                @endphp
 
                 <img class="img1" src="/assets/{{ $product['frontImage'] }}" alt="">
                 <img class="img2" src="/assets/{{ $product['backImage'] }}" alt="1b">
+                @foreach ($product['badges'] as $badge)
+                    @if ($badge['type'] == 'discount')
+                        <span v-if="product.discount" class="discount">{{ $badge['value'] }}</span>
+                    @endif
+                @endforeach
 
-                <span v-if="product.discount" class="discount">{{ $product['name'] }}</span>
 
                 <span class="heart">&hearts;</span>
 
                 <div class="descr">
                     <span class="marca">{{ $product['brand'] }}</span>
-                    <h3>{{ $product['brand'] }}</h3>
-                    <span class="red-price">&euro;{{ $product['price'] }}</span>
-                    <span v-if="product.fullPrice" class="barred-price">&euro;{{ $product['price'] }}</span>
+                    <h3>{{ $product['name'] }}</h3>
+                    <span class="red-price">&euro;{{ $format_price_disc }}</span>
+                    <span class="barred-price">&euro;{{ $product['price'] }}</span>
                 </div>
 
             </div>
